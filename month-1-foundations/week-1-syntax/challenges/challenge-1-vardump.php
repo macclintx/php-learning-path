e1c0155e2da64b5ff79ba8053424bcde50c0046e
macclintx <macclintx@gmail.com>
2026-07-31 09:00:19 +0300

    week1: challenge 1 - print 5 variables with vardump

---
+++ month-1-foundations/week-1-syntax/challenges/challenge-1-vardump.php
@@ -0,0 +1,16 @@
+<?php
+    $name = 'dk';
+    $age = 25;
+    $salary = 200000.23;
+    $is_employee = true;
+    $roles = ['admin', 'cybersec', 'backend'];
+
+    var_dump($name);
+    echo "\n";
+    var_dump($age);
+    echo "\n";
+    var_dump($salary);
+    echo "\n";
+    var_dump($is_employee);
+    echo "\n";
+    var_dump($roles);
