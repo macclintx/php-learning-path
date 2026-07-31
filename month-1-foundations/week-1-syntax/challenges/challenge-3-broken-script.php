8a88687ffd060a25ac30b6cef311d43517b22f9a
macclintx <macclintx@gmail.com>
2026-07-31 18:09:55 +0300

    week1: challenge 3 - break code intentionally to see errors

---
+++ month-1-foundations/week-1-syntax/challenges/challenge-3-broken-script.php
@@ -0,0 +1,8 @@
+<?php
+    $balance = 2999;
+
+    echo "$balanc";
+    //error is Undefined variable $balanc
+
+    echo "Checking for errors"
+    //error is syntax error, enexpected end of file, expecting "," or ";"
