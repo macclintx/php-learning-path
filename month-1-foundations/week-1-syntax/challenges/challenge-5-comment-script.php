273556e6b21f9d93c9305ab3c0288ebdb21af2ce
macclintx <macclintx@gmail.com>
2026-08-01 20:42:26 +0300

    week1: challenge 5 - meaningful commnents on a script

--- month-1-foundations/week-1-syntax/challenges/challenge-5-comment-script.php
+++ month-1-foundations/week-1-syntax/challenges/challenge-5-comment-script.php
@@ -1,5 +1,14 @@
 <?php
-    $pass = 'pass123';
 
-    //check if pass is valid format
-    var_dump($pass);
+    $occupation = 'Doctor';
+
+    //salary need cent precision for accuracy in calculations of deductions and formating
+    $salary = 40000.1234;
+    $hospital_branch = 'Kileleshwa';
+
+    echo "He is a $occupation at $hospital_branch branch, earning KES $salary\n";
+
+    print "The branch in $hospital_branch has a $occupation being paid KES $salary\n";
+
+    //use for formatting decimal points
+    printf('Its very wrong for a %s working at a hospital in %s be paid %.2f. Its just too low',$occupation,$hospital_branch,$salary);
